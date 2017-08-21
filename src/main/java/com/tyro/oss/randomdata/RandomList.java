bea6484f9f55fa5bad4b58bd4739daafbad320f5
Gary Teh and Stephen O'Rourke <just-dev+cteh+sorourke@tyro.com>
2017-08-21 15:09:21 +1000

    Implement random list

---
+++ src/main/java/com/tyro/oss/randomdata/RandomList.java
@@ -0,0 +1,40 @@
+/*
+ * #%L
+ * random-data
+ * %%
+ * Copyright (C) 2016 - 2017 Tyro Payments Pty Ltd
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.tyro.oss.randomdata;
+
+import static com.tyro.oss.randomdata.RandomInteger.randomIntegerBetween;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class RandomList {
+
+    public static <T> List<T> randomList(Supplier<T> supplier) {
+        return randomList(supplier, randomIntegerBetween(1, 20));
+    }
+
+    public static <T> List<T> randomList(Supplier<T> supplier, int size) {
+        return range(0, size)
+                .mapToObj(i -> supplier.get())
+                .collect(toList());
+    }
+}
