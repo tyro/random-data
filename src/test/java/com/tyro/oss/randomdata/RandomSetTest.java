2f5a647b639bb1aa27ca2c3992c68b94b39403f6
Gary Teh and Stephen O'Rourke <just-dev+cteh+sorourke@tyro.com>
2017-08-21 15:46:12 +1000

    Implement random set

---
+++ src/test/java/com/tyro/oss/randomdata/RandomSetTest.java
@@ -0,0 +1,41 @@
+/*
+ * #%L
+ * random-data
+ * %%
+ * Copyright (C) 2016 - 2017 Tyro Payments Pty Ltd
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.tyro.oss.randomdata;
+
+import static com.tyro.oss.randomdata.RandomSet.randomSet;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class RandomSetTest {
+
+    @Test
+    public void shouldReturnRandomSetOfString() throws Exception {
+        assertThat(randomSet(RandomString::randomString))
+                .hasOnlyElementsOfTypes(String.class);
+    }
+
+    @Test
+    public void shouldReturnRandomSetOfStringWithSizeOf255() throws Exception {
+        assertThat(randomSet(RandomString::randomString, 255))
+                .hasSize(255)
+                .hasOnlyElementsOfTypes(String.class);
+    }
+}
