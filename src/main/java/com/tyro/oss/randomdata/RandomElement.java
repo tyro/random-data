2dd3e6db86cc77fe46076b1fa4e6e3c0b5753cd8
Gary Teh and Stephen O'Rourke <just-dev+cteh+sorourke@tyro.com>
2017-12-19 16:41:30 +1100

    Implement random element

---
+++ src/main/java/com/tyro/oss/randomdata/RandomElement.java
@@ -0,0 +1,52 @@
+/*
+ * #%L
+ * random-data
+ * %%
+ * Copyright (C) 2016 - 2017 Tyro Payments Pty Ltd
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.tyro.oss.randomdata;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.function.Supplier;
+
+import static com.tyro.oss.randomdata.RandomInteger.randomIntegerBetween;
+
+public class RandomElement {
+
+    @SafeVarargs
+    public static <T> T randomElementOf(T object, T... objects) {
+        int index = randomIntegerBetween(0, objects.length);
+        if (index == 0) {
+            return object;
+        }
+        return objects[index - 1];
+    }
+
+    public static <T> T randomElementOf(Collection<T> objects) {
+        int index = randomIntegerBetween(0, objects.size() - 1);
+        return new ArrayList<>(objects).get(index);
+    }
+
+    @SafeVarargs
+    public static <T> T randomElementOf(Supplier<T> object, Supplier<T>... objects) {
+        int index = randomIntegerBetween(0, objects.length);
+        if (index == 0) {
+            return object.get();
+        }
+        return objects[index - 1].get();
+    }
+}
---
+++ src/test/java/com/tyro/oss/randomdata/RandomElementTest.java
@@ -0,0 +1,61 @@
+/*
+ * #%L
+ * random-data
+ * %%
+ * Copyright (C) 2016 - 2017 Tyro Payments Pty Ltd
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.tyro.oss.randomdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static com.tyro.oss.randomdata.RandomElement.randomElementOf;
+import static com.tyro.oss.randomdata.RandomInteger.randomInteger;
+import static com.tyro.oss.randomdata.RandomString.randomString;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(RepeatRunner.class)
+public class RandomElementTest {
+
+    @Test
+    public void shouldReturnRandomElementOfVarargs() throws Exception {
+        String str1 = randomString();
+        String str2 = randomString();
+        assertThat(randomElementOf(str1)).isEqualTo(str1);
+        assertThat(randomElementOf(str1, str2)).isIn(str1, str2);
+    }
+
+    @Test
+    public void shouldReturnRandomElementOfCollections() throws Exception {
+        int int1 = randomInteger();
+        int int2 = randomInteger();
+        List<Integer> integers = asList(int1, int2);
+
+        assertThat(randomElementOf(integers)).isIn(int1, int2);
+    }
+
+    @Test
+    public void shouldReturnRandomElementOfSuppliers() throws Exception {
+        String str1 = randomString();
+        String str2 = randomString();
+
+        assertThat(randomElementOf(() -> str1)).isEqualTo(str1);
+        assertThat(randomElementOf(() -> str1, () -> str2)).isIn(str1, str2);
+    }
+}
